c9760ab85422b52ae961de3f6aa6a11779c93078
yaokuku123 <yaoqijun@outlook.com>
2020-08-18 20:40:14 +0800

    构建Mybatis的基于xml的crud

--- mybatis-xml-crud/src/main/java/com/yqj/dao/UserMapper.java
+++ mybatis-xml-crud/src/main/java/com/yqj/dao/UserMapper.java
@@ -1,8 +1,10 @@
 package com.yqj.dao;
 
 import com.yqj.pojo.User;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
+import java.util.Map;
 
 /**
  * Copyright(C),2019-2020,XXX公司
@@ -11,5 +13,24 @@ import java.util.List;
  * Date: 2020/8/18 20:08
  */
 public interface UserMapper {
+    //查询所有
     List<User> selectUser();
+
+    //根据id查询用户
+    User selectById(int id);
+
+    //添加用户
+    int addUser(User user);
+
+    //修改用户
+    int updateUser(User user);
+
+    //删除用户
+    int deleteUser(int id);
+
+    //根据用户名和密码查询(传递两个参数)
+    User selectByNameAndPwd(@Param("name") String name,@Param("pwd") String pwd);
+
+    //根据用户名和密码查询(万能Map)
+    User selectByMap(Map<String,Object> map);
 }
--- mybatis-xml-crud/src/test/java/com/yqj/test/MyTest.java
+++ mybatis-xml-crud/src/test/java/com/yqj/test/MyTest.java
@@ -6,7 +6,9 @@ import com.yqj.utils.MybatisUtils;
 import org.apache.ibatis.session.SqlSession;
 import org.junit.Test;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Copyright(C),2019-2020,XXX公司
@@ -16,14 +18,80 @@ import java.util.List;
  */
 public class MyTest {
 
+    //查询所有
     @Test
-    public void test(){
+    public void testSelectAll(){
         SqlSession session = MybatisUtils.getSession();
         UserMapper mapper = session.getMapper(UserMapper.class);
         List<User> users = mapper.selectUser();
         for (User user : users) {
             System.out.println(user);
         }
+        session.close();
+    }
+
+    //根据id查询用户
+    @Test
+    public void testSelectById(){
+        SqlSession session = MybatisUtils.getSession();
+        UserMapper mapper = session.getMapper(UserMapper.class);
+        User user = mapper.selectById(1);
+        System.out.println(user);
+        session.close();
+    }
+
+    //添加用户
+    @Test
+    public void testAddUser(){
+        SqlSession session = MybatisUtils.getSession();
+        UserMapper mapper = session.getMapper(UserMapper.class);
+        mapper.addUser(new User(4,"tom","123"));
+        session.commit();
+        session.close();
+    }
+
+    //修改用户
+    @Test
+    public void testUpdateUser(){
+        SqlSession session = MybatisUtils.getSession();
+        UserMapper mapper = session.getMapper(UserMapper.class);
+        User user = mapper.selectById(1);
+        user.setName("aliceUpdate");
+        mapper.updateUser(user);
+        session.commit();
+        session.close();
+    }
+
+    //删除用户
+    @Test
+    public void testDeleteUser(){
+        SqlSession session = MybatisUtils.getSession();
+        UserMapper mapper = session.getMapper(UserMapper.class);
+        mapper.deleteUser(4);
+        session.commit();
+        session.close();
+    }
+
+    //根据用户名和密码查询(传递两个参数)
+    @Test
+    public void testSelectByNameAndPwd(){
+        SqlSession session = MybatisUtils.getSession();
+        UserMapper mapper = session.getMapper(UserMapper.class);
+        User user = mapper.selectByNameAndPwd("alice", "123456");
+        System.out.println(user);
+        session.close();
+    }
+
+    //根据用户名和密码查询(万能Map)
+    @Test
+    public void testSelectByMap(){
+        SqlSession session = MybatisUtils.getSession();
+        UserMapper mapper = session.getMapper(UserMapper.class);
+        Map<String,Object> map = new HashMap<>();
+        map.put("username","alice");
+        map.put("password","123456");
+        User user = mapper.selectByMap(map);
+        System.out.println(user);
         session.close();
     }
 }
